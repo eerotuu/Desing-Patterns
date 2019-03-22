7f9ef034531fa5c66cbd9af386852d9586ff03c6
eerotuu <eerotuu@metropolia.fi>
2019-03-22 19:38:19 +0200

    added factory creation with reflection api

--- abstractfactory/src/abstractfactory/Main.java
+++ abstractfactory/src/abstractfactory/Main.java
@@ -6,6 +6,12 @@
 package abstractfactory;
 
 import abstractfactory.outfitfactory.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,14 +20,36 @@ import abstractfactory.outfitfactory.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         OutfitFactory factory = new AdidasOutfitFactory();
         Outfit myOutfit = new Outfit();
         myOutfit.createOutfit(factory);
-        System.out.println(myOutfit+"\n");
-        
+        System.out.println(myOutfit + "\n");
+
         factory = new BossOutfitFactory();
         myOutfit.createOutfit(factory);
         System.out.println(myOutfit);
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileInputStream("src/abstractfactory/factory.properties"));
+            Class cl = Class.forName("abstractfactory.outfitfactory."
+                    + prop.getProperty("factory"));
+            factory = (OutfitFactory) cl.newInstance();
+            myOutfit.createOutfit(factory);
+            System.out.println("\n" + myOutfit);
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
